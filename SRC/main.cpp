b8084c7c6ed4b8c309a81c0cffd84ac5a7adf50a
jvrnogueira <jvrn321@gmail.com>
2026-05-28 11:37:13 -0300

    Update main.cpp
    add first class

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
 
+class item{
+private:
+  int id_;
+  std::string name_;
+  double weight_;
+
+public:
+// Construtor
+item(int id, std::string name, double weight)
+  :id_(id), name_(name), weight_(weight) {}
+
+//Getters
+int get_id() const{return id_;}
+std::string get_name() const {return name_;}
+double get_weight() const {return weight_:}
+}
